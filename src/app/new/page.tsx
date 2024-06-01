1630faf4df68d7ab261d6d1b5fb400c87e624eb9
tiago <tiago.zdo@gmail.com>
2024-05-31 21:21:41 -0300

    React form

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,8 +1,46 @@
+"use client";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 function NewPage() {
+  const router = useRouter();
+  const { handleSubmit, register } = useForm();
+  const onSubmit = handleSubmit(async (data) => {
+    const res = await axios.post("/api/tasks", data);
+    router.push("/");
+  });
+
   return (
-    <div>NewPage</div>
-  )
+    <section className="h-screen flex items-center justify-center">
+      <form onSubmit={onSubmit}>
+        <label htmlFor="title" className="font-bold text-xs">
+          Write your title:
+        </label>
+        <input
+          id="title"
+          type="text"
+          placeholder="Write a title"
+          className="px-3 py-1 border border-gray-300 rounded-md shadow-xs
+          focus:outline-none focus:ring-1 focus:ring-sky-300 focus:border-sky-300 text-black block mb-2"
+          {...register("title")}
+        />
+        <label htmlFor="description" className="font-bold text-xs">
+          Write your description:
+        </label>
+        <textarea
+          id="description"
+          placeholder="Write a description"
+          className="px-3 py-1 border border-gray-300 rounded-md shadow-sm
+        focus:outline-none focus:ring-1 focus:ring-sky-300 focus:border-sky-300 text-black block w-full"
+          {...register("description")}
+        ></textarea>
+        <button className="bg-sky-500 px-3 py-1 rounded-md text-white mt-2">
+          Create
+        </button>
+      </form>
+    </section>
+  );
 }
 
 export default NewPage;
